05e5cb9f9c74a38e71709999d6e3dec96f5c2c1c
MercyMilkahNjokiKahungu <mercy.njoki@student.moringaschool.com>
2022-08-27 14:10:00 +0300

    done assignmnent

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //iterate from beinning to the middle
+  for (i=0;i < word.length/2;i++){
+    //check each letter to the corresponding letter from the end
+    const j = word.length - 1 - i;
+    if(word.charAt(i) !== word.charAt(j)){
+      return false;
+    }
+  }
+  return true;
 }
+//if we reach the middle and all the letters match, return true
+
 
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+  if we reach the middle, and all the letters match, return true
 */
 
 /*
-  Add written explanation of your solution here
+  testcase 
+  a bb a
+  *    *
+    **
+    return true 
 */
 
 // You can run `node index.js` to view these console logs
